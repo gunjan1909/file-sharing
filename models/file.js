0f7eb867008360b033dfe7807b79e8a65240660c
gunjan1909 <bhanarkaranita@gmail.com>
2022-09-18 18:26:47 +0530

    video completed

---
+++ models/file.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const File = new mongoose.Schema({
+  path: {
+    type: String,
+    requied: true,
+  },
+  originalName: {
+    type: String,
+    required: true,
+  },
+  password: String,
+  downloadCount: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+mongoose.model("File", File);
+
+module.exports = mongoose.model("File", File);
